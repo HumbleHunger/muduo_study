297355a5c9601f0bd1086c876d6ac1d56c8606c1
HumbleHunger <2495970924@qq.com>
2020-03-28 22:34:38 +0800

    update

---
+++ word.c
@@ -0,0 +1,29 @@
+/*************************************************************************
+	> File Name: word.c
+	> Author: 
+	> Mail: 
+	> Created Time: 2020年03月28日 星期六 22时04分01秒
+ ************************************************************************/
+
+#include<stdio.h>
+int main()
+{
+    int p=1;
+    while(p<=3){
+        printf("6");
+        p++;
+    }
+    p=1;
+    printf("\n");
+    do{
+        printf("6");
+        p++;
+    }while(p<=3);
+    printf("\n");
+    while(0){
+        printf("while\n");
+    }
+    do{
+        printf("do_while\n");
+    }while(0);
+}
