49c24468f30a5e32d1a5fec7e19f96c2cb2802f1
HumbleHunger <2495970924@qq.com>
2020-04-07 20:04:34 +0800

    update

--- 6.c
+++ 6.c
@@ -8,5 +8,16 @@
 #include<stdio.h>
 int main()
 {
-
+    int a,sum,sum_all;
+    a=sum=sum_all=0;
+    for(int i=1;i<=13;i++){
+        for(int j=0;j<23;j++){
+            scanf("%d",&a);
+            sum+=a;
+        }
+        printf("第%d个输入地累计确诊数为%d\n",i,sum);
+        sum_all+=sum;
+    }
+    printf("13个输入地累计确诊总个数为%d\n",sum_all);
+    return 0;
 }
