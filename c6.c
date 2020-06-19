c0e5eb088bb69dbcc8efd9bddfbcd3342d26894c
HumbleHunger <2495970924@qq.com>
2020-06-19 21:40:39 +0800

    update

--- c6.c
+++ c6.c
@@ -4,10 +4,31 @@
 	> Mail: 
 	> Created Time: 2020年06月15日 星期一 07时51分17秒
  ************************************************************************/
-
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
+void sort(char *p_s1, char *p_s2);
 int main()
 {
+	char s1[100], s2[100], s3[100];
+	char *p_s1=s1,*p_s2=s2,*p_s3=s3;
+	printf("请随意输入三个字符串：\n");
+	gets(s1);
+	gets(s2);
+	gets(s3);
+	sort(p_s1, p_s2);
+	sort(p_s1, p_s3);
+	sort(p_s2, p_s3);
+	printf("按照由小到大的顺序输出如下：\n%s\t%s\t%s\t", p_s1, p_s2, p_s3);
+	return 0;
+}
 
+void sort(char *p_s1, char *p_s2)
+{
+	char temp[100];
+	if(strcmp(p_s1,p_s2)>0)
+	{
+		strcpy(temp, p_s1);
+		strcpy(p_s1, p_s2);
+		strcpy(p_s2, temp);
+	}
 }
--- c7.c
+++ c7.c
@@ -4,9 +4,68 @@
 	> Mail: 
 	> Created Time: 2020年06月15日 星期一 23时56分42秒
  ************************************************************************/
-
-#include<stdio.h>
+#include <stdio.h>
+typedef struct student
+{
+	long studentID;      	 /*学号*/
+	char studentName[10];	/*姓名*/ 
+	char studentSex;		/*性别*/ 
+	int score[3];			/*三门课的成绩*/ 
+}STUDENT;
+void Input(STUDENT stu[],int n,int m);  /*声明输入函数*/ 
+void Aver(STUDENT stu[],float aver[],int n,int m);	/*声明计算平均数函数*/ 
+void Print(STUDENT stu[],float aver[],int n,int m);	/*声明输出函数*/ 
 int main()
 {
-
+	float aver[5]; 	/*定义平均数变量类型*/ 
+	STUDENT stu[5];		/*定义结构体变量stu[i]*/
+	Input(stu,5,3); 	/*调用输入函数*/
+	Aver(stu,aver,5,3);		/*调用计算平均数函数*/
+	Print(stu,aver,5,3);	/*调用输出函数*/
+	return 0;
+}
+void Input(STUDENT stu[],int n,int m)
+{
+	int i,j;
+	/*利用循环输入5个学生的信息*/
+	for(i=0;i<n;i++)
+	{
+		printf("请输入第%d个学生的学号、姓名、性别(用M或F表示)、成绩：\n",i+1); /*提示用户使用键盘输入信息*/
+		scanf("%ld %s %c",&stu[i].studentID,stu[i].studentName,&stu[i].studentSex);
+		/*利用循环将学生成绩输入stu.score[i]数组*/ 
+		for(j=0;j<m;j++)
+		{
+			scanf("%d",&stu[i].score[j]);
+		}
+	}
+} 
+void Aver(STUDENT stu[],float aver[],int n,int m)
+{
+	int i,j,sum[n];
+	for(i=0;i<n;i++)
+	{
+		sum[i]=0;/*初始化sum[i]为0*/
+		/*计算三科成绩总分*/ 
+		for(j=0;j<m;j++)
+		{
+			sum[i]=sum[i]+stu[i].score[j];
+		}
+		aver[i]=(float)sum[i]/m;/*计算平均分*/ 
+	}
+}
+/**输出学生信息*/
+void Print(STUDENT stu[],float aver[],int n,int m)
+{
+	int i,j;
+	for(i=0;i<n;i++)
+	{
+		printf("%10ld%8s%3c",stu[i].studentID,
+							stu[i].studentName,
+							stu[i].studentSex);
+		for(j=0;j<m;j++)
+		{
+			printf("%4d",stu[i].score[j]);
+		}
+		printf("%6.1f\n",aver[i]);
+	}
 }
