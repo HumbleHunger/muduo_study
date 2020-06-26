3f717f533e3c70f16012bcefced216f962927b61
HumbleHunger <2495970924@qq.com>
2020-06-26 20:36:03 +0800

    update

--- c8.c
+++ c8.c
@@ -4,9 +4,35 @@
 	> Mail: 
 	> Created Time: 2020年06月17日 星期三 07时56分06秒
  ************************************************************************/
-
 #include<stdio.h>
+#include<string.h>
 int main()
 {
-    FILE* file=fopen("1",w);
+        int i,a=3;
+        char b[50];
+        FILE *p1;
+        printf("输入明文");
+        gets(b);
+        for(i=0;i<strlen(b);i++)
+        {
+        	b[i]=b[i]+a;
+            if(b[i]>122){
+                b[i]=b[i]-26;
+            }
+        }
+        p1=fopen("1","w");
+        fputs(b,p1);
+        fclose(p1);
+        printf("密文已保存于文件1中\n");
+        for(i=0;i<strlen(b);i++)
+        {
+            b[i]=b[i]-a;
+            if(b[i]<97){
+                b[i]=b[i]+26;
+            }
+        }
+        p1=fopen("2","w");
+        fputs(b,p1);
+        fclose(p1);
+        printf("明文已保存于文件2中\n");
 }
