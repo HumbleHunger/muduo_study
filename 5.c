a3efbfe3a2ab201894e8166b6ed5f81f8cf7a58b
HumbleHunger <2495970924@qq.com>
2020-07-03 22:57:27 +0800

    update

--- 5.c
+++ 5.c
@@ -26,21 +26,6 @@ void jiami(char *my)
     char *c=(char *)malloc(strlen(a)+1);
     memset(c,0,sizeof(c));
     for(int i=0;i<strlen(a);i++){
-        c[i]=b[i]+a[i]-65;
-        if(c[i]>90){
-            c[i]=c[i]-26;
-        }
-    }
-    printf("明文:%s\n密钥:%s\n密文:%s\n",a,b,c);
-    free(a);
-    free(b);
-    free(c);
-}
-void jiemi(char *my)
-{
-    char *a=(char *)malloc(101);
-    memset(a,0,sizeof(a));
-    printf("请输入密文\n");
     scanf("%s",a);
     char *b=(char *)malloc(strlen(a)+1);
     memset(b,0,sizeof(b));
