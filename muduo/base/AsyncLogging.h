ecc0eb8b04e46f694081b55985c82c024450f6aa
HumbleHunger <2495970924@qq.com>
2021-04-14 21:14:53 +0800

    log

--- muduo/base/AsyncLogging.h
+++ muduo/base/AsyncLogging.h
@@ -22,7 +22,7 @@ namespace muduo
 class AsyncLogging : noncopyable
 {
  public:
-
+//异步日志系统
   AsyncLogging(const string& basename,
                off_t rollSize,
                int flushInterval = 3);
